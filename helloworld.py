768148ee7cb2c47c5c7a5b15d7a503aa0d7acb55
Shai Ben-Naphtali <shai@shaibn.com>
2011-05-31 11:29:59 +0300

    Updated to print XX times

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
-print "Hello, World!"
+
+# ./helloworld.py 10
+# Will print Hello World 10 times
+
+import re, string, sys
+
+counter = int(sys.argv[1])
+
+# Option 2 (much simpler)
+for i in xrange(1,counter+1):
+	print "Hello, World %d!" %(i,)
