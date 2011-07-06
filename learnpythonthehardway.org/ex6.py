627a009f9231516fc1d1caf12bd533e707490b9f
Shai Ben-Naphtali <shai@shaibn.com>
2011-07-06 13:39:21 +0300

    Extra credits #2, #3 and #4

--- learnpythonthehardway.org/ex6.py
+++ learnpythonthehardway.org/ex6.py
@@ -1,3 +1,5 @@
+# http://learnpythonthehardway.org/book/ex6.html
+
 # This sets the entire sentence with the substitution into 'x' variable
 x = "There are %d types of people." % 10
 # Set the variable 'binary' with the string 'binary'
@@ -5,7 +7,7 @@ binary = "binary"
 # Set the variable below with the string "don't"
 do_not = "don't"
 # Set 'y' with the text after the substitution
-y = "Those who know %s and those who %s." % (binary, do_not)
+y = "Those who know %s and those who %s." % (binary, do_not) # Extra credit #2
 
 # Print 'x' string
 print x
@@ -13,14 +15,14 @@ print x
 print y
 
 # Print the text as a string after the substitution
-print "I said: %r." % x
+print "I said: %r." % x # Extra credit #2
 # Print the text as a string after the substitution
-print "I also said: '%s'." % y
+print "I also said: '%s'." % y # Extra credit #2
 
 # Set the variable 'hilarious' with the string 'False'
 hilarious = False
 # Set the variable as a whole without the subsitution
-joke_evaluation = "Isn't that joke so funny?! %r"
+joke_evaluation = "Isn't that joke so funny?! %r" # Extra credit #2
 
 # This line actually does the substitution from the last two lines above
 print joke_evaluation % hilarious
@@ -31,4 +33,7 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 # Print both strings
-print w + e
+# Extra credit #4: Taking two strings and using the plus (+) sign will
+# make them a long string. Taking two integers on the other hand (1 + 2 = 3)
+# would do addition. This is an example of polymorphism.
+print w + e # Extra credit #3
